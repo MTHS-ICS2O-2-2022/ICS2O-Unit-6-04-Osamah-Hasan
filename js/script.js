8e4f632270660265ede8888f385f5d60b1cfe245
Osamah_Hasan <124389134+Osamah-Hasan@users.noreply.github.com>
2023-05-31 17:01:06 +0000

    added icon change html added web download added sw.js change css style add image change script

--- js/script.js
+++ js/script.js
@@ -1,5 +1,33 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Osamah Hasan All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Osamah Hasan
+// Created on: Feb 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * Check service worker support"
+*/
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit-6-01-Osamah-Hasan/sw.js", {
+    scope: "/ICS2O-Unit-6-01-Osamah-Hasan/",
+  })
+}
+
+window.onload = function () {
+
+  const params = new URLSearchParams(window.location.search)
+
+  // input
+  const radius = params.get("r")
+  console.lag(radius)
+
+  // process
+  const volume = (4 / 3) * Math.PI * Math.pow(radius, 3)
+  const roundedVolume = volume.toFixed(2)
+
+  // output
+  document.getElementById("dimenstion").innerHTML = "r = " + radius
+  document.getElementById("output").innerHTML = "V = " + roundedVolume + "cm³"
+}
